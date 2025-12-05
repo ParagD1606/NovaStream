3c4eb263acf901a098ad6ba845f2663fa2a26e8d
ParagD1606 <paragdharamkar2006@gmail.com>
2025-12-06 00:23:25 +0530

    postman issue

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,22 @@
+// Ensure this file looks like this:
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js"; 
 
 const router = Router();
 
-router.route('/register').post(registerUser);
-
+router.route('/register').post(
+    upload.fields([ // <--- THIS IS THE CRITICAL MISSING PIECE
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+);
 
 export default router;
